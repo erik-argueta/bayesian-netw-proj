471a48c15c6727f4bfda4afb5a12e94c852f33c9
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-09-21 10:52:46 -0700

    removed main and added hints

--- diagnostics.py
+++ diagnostics.py
@@ -9,5 +9,12 @@ class Diagnostics:
         ... # placeholder for the student's code, to be replaced by the student
 
     def diagnose (self, asia, smoking, xray, dyspnea):
+        '''
+        Hints: 
+            - Class should make use of either 'BayesNet' or 'DiscreteBayesNet'
+                - DiscreteBayesNet handles multiple variables that can be named anything
+            - Use the 'enumeration inference' algo to calculate the probability of each disease 
+                - This algo is implemented as 'enumeration_ask_function' in textbook
+        '''
         # To be implemented by the student
         return ["the disease", -1.0] # placeholder return value, to be replaced by the student
